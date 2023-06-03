fb848c41f73f5f628e4b47e8fcb1629159266b51
zukimania <eayodele585@gmail.com>
2023-06-03 08:44:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+  const text = document.querySelector(".sec-text");
+
+        const textLoad = () => {
+            setTimeout(() => {
+                text.textContent = "To My";
+            }, 0);
+            setTimeout(() => {
+                text.textContent = "UI/UX";
+            }, 4000);
+            setTimeout(() => {
+                text.textContent = "Portfolio";
+            }, 8000); //1s = 1000 miliseconds
+        }
+
+
+        textLoad();
+        setInterval(textLoad, 12000);
