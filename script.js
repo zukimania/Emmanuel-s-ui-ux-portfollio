8e39bde98d5b348f576b7c4f0da2d8d9f272e7bd
zukimania <eayodele585@gmail.com>
2023-09-16 15:25:08 +0100

    first commit

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-  const text = document.querySelector(".sec-text");
+//   const text = document.querySelector(".sec-text");
 
-        const textLoad = () => {
-            setTimeout(() => {
-                text.textContent = "To My";
-            }, 0);
-            setTimeout(() => {
-                text.textContent = "UI/UX";
-            }, 4000);
-            setTimeout(() => {
-                text.textContent = "Portfolio";
-            }, 8000); //1s = 1000 miliseconds
-        }
+//         const textLoad = () => {
+//             setTimeout(() => {
+//                 text.textContent = "To My";
+//             }, 0);
+//             setTimeout(() => {
+//                 text.textContent = "UI/UX";
+//             }, 4000);
+//             setTimeout(() => {
+//                 text.textContent = "Portfolio";
+//             }, 8000); //1s = 1000 miliseconds
+//         }
 
 
-        textLoad();
-        setInterval(textLoad, 12000);
+//         textLoad();
+//         setInterval(textLoad, 12000);
 
-        // import anime from 'animejs/lib/anime.es.js';
+//         // import anime from 'animejs/lib/anime.es.js';
 
 
